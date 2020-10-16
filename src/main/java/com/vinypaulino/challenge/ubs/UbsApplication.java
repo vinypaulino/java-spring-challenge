3a269e828e7950192f96fd5ab81a7cb0ff4c72b1
Vinicius Paulino <vinicius.paulino@softplan.com.br>
2020-10-16 20:57:53 -0300

    First commit

---
+++ src/main/java/com/vinypaulino/challenge/ubs/UbsApplication.java
@@ -0,0 +1,13 @@
+package com.vinypaulino.challenge.ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsApplication.class, args);
+	}
+
+}
