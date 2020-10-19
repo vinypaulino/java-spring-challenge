d69451b3f6dc4d168baa3c0da6faf8dbe5cecf4c
Vinicius Paulino <vinicius.paulino@softplan.com.br>
2020-10-19 08:26:05 -0300

    add calc for distance

--- src/main/java/com/vinypaulino/challenge/ubs/domain/model/UnidadeBasicaDeSaude.java
+++ src/main/java/com/vinypaulino/challenge/ubs/domain/model/UnidadeBasicaDeSaude.java
@@ -25,5 +25,22 @@ public class UnidadeBasicaDeSaude {
 	
 	@Embedded
 	private Scores scores;
+	
+	public double distance(double lat1, double lon1  ) {
+
+	    final int R = 6371; // Radius of the earth
+
+	    double latDistance = Math.toRadians(this.geoCode.getLatitude() - lat1);
+	    double lonDistance = Math.toRadians(this.geoCode.getLongitude() - lon1);
+	    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+	            + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(this.geoCode.getLatitude()))
+	            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    double distance = R * c * 1000; // convert to meters
+
+	    distance = Math.pow(distance, 2);
+
+	    return Math.sqrt(distance);
+	}
 
 }
